83ec6d161033af5e7fcfb7d2e172cd61e03b32ad
Muwonge Hassan Saava <hassansaava@gmail.com>
2023-08-09 20:28:48 +0300

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domains;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Auckland Region",
+                    Code="AKL",
+                    RegionImageUrl="testing"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+       public IActionResult GetStudents()
+        {
+            string[] studentNames = new string[] { "saava", "denis" };
+
+            return Ok(studentNames);
+
+        }
+    }
+}
