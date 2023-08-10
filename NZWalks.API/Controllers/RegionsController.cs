93f022fcf79cd81a6e1ab0d87b8507c10aa7e1d5
Muwonge Hassan Saava <hassansaava@gmail.com>
2023-08-10 09:29:19 +0300

    added dto modals to update and delete a record

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
 using NZWalks.API.Models.Domains;
 
 namespace NZWalks.API.Controllers
@@ -8,21 +9,36 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZWalksDbContext _dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+
+        }
+        
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                new Region
-                {
-                    Id=Guid.NewGuid(),
-                    Name="Auckland Region",
-                    Code="AKL",
-                    RegionImageUrl="testing"
-                }
-            };
-
+            var regions = _dbContext.Regions.ToList();
             return Ok(regions);
+        }
+        
+        // GET SINGLE REGION (Get Region by ID)
+        //GET: https://localhost:portNumber/api/regions/id
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = _dbContext.Regions.Find(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+
         }
     }
 }
